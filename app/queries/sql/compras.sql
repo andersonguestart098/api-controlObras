e5e275214a7b19d5f51fb96b17f35ea24ba79554
Anderson Henrique Tavares Anastácio <anderson.guestart98@gmail.com>
2026-08-05 10:01:02 -0300

    ajuste sql comrpas

--- app/queries/sql/compras.sql
+++ app/queries/sql/compras.sql
@@ -1,56 +1,3 @@
-WITH CUSTO_ATUAL AS (
-    SELECT
-        CUS.CODEMP,
-        CUS.CODPROD,
-
-        MAX(CUS.CUSSEMICM)
-            KEEP (
-                DENSE_RANK LAST
-                ORDER BY CUS.DTATUAL
-            ) AS CUSSEMICM
-
-    FROM TGFCUS CUS
-
-    GROUP BY
-        CUS.CODEMP,
-        CUS.CODPROD
-),
-
-CUSTO_NOTA AS (
-    SELECT
-        ITE.NUNOTA,
-
-        ROUND(
-            SUM(
-                NVL(ITE.QTDNEG, 0)
-                * NVL(CUS.CUSSEMICM, 0)
-            ),
-            2
-        ) AS CUSTO_MEDIO_SEM_ICMS_TOTAL
-
-    FROM TGFITE ITE
-
-    INNER JOIN TGFCAB CAB_CUSTO
-            ON CAB_CUSTO.NUNOTA = ITE.NUNOTA
-
-    LEFT JOIN CUSTO_ATUAL CUS
-           ON CUS.CODEMP = CAB_CUSTO.CODEMP
-          AND CUS.CODPROD = ITE.CODPROD
-
-    WHERE CAB_CUSTO.CODTIPOPER = 1301
-
-      /* Exclui tipo de negociação Interno Obras */
-      AND NVL(
-            CAB_CUSTO.CODTIPVENDA,
-            0
-          ) <> 323
-
-      AND CAB_CUSTO.CODPROJ = {{CODPROJ}}
-
-    GROUP BY
-        ITE.NUNOTA
-)
-
 SELECT
     CAB.NUNOTA,
     CAB.NUMNOTA,
@@ -71,11 +18,25 @@ SELECT
     CAB.CODTIPVENDA,
     TPV.DESCRTIPVENDA AS TIPO_NEGOCIACAO,
 
-    NVL(CAB.VLRNOTA, 0) AS VLRNOTA,
+    NVL(
+        CAB.VLRNOTA,
+        0
+    ) AS VLRNOTA,
 
-    NVL(CAB.VLRICMS, 0) AS VLRICMS,
-    NVL(CAB.VLRPIS, 0) AS VLRPIS,
-    NVL(CAB.VLRCOFINS, 0) AS VLRCOFINS,
+    NVL(
+        CAB.VLRICMS,
+        0
+    ) AS VLRICMS,
+
+    NVL(
+        CAB.VLRPIS,
+        0
+    ) AS VLRPIS,
+
+    NVL(
+        CAB.VLRCOFINS,
+        0
+    ) AS VLRCOFINS,
 
     NVL(
         CST.CUSTO_MEDIO_SEM_ICMS_TOTAL,
@@ -158,15 +119,69 @@ LEFT JOIN TGFPAR PAR
 LEFT JOIN TCSPRJ PRJ
        ON PRJ.CODPROJ = CAB.CODPROJ
 
-LEFT JOIN CUSTO_NOTA CST
+LEFT JOIN (
+    SELECT
+        ITE.NUNOTA,
+
+        ROUND(
+            SUM(
+                NVL(ITE.QTDNEG, 0)
+                * NVL(CUS.CUSSEMICM, 0)
+            ),
+            2
+        ) AS CUSTO_MEDIO_SEM_ICMS_TOTAL
+
+    FROM TGFITE ITE
+
+    INNER JOIN TGFCAB CAB_CUSTO
+            ON CAB_CUSTO.NUNOTA = ITE.NUNOTA
+
+    LEFT JOIN (
+        SELECT
+            C.CODEMP,
+            C.CODPROD,
+
+            MAX(C.CUSSEMICM)
+                KEEP (
+                    DENSE_RANK LAST
+                    ORDER BY C.DTATUAL
+                ) AS CUSSEMICM
+
+        FROM TGFCUS C
+
+        GROUP BY
+            C.CODEMP,
+            C.CODPROD
+    ) CUS
+           ON CUS.CODEMP = CAB_CUSTO.CODEMP
+          AND CUS.CODPROD = ITE.CODPROD
+
+    WHERE CAB_CUSTO.CODTIPOPER = 1301
+
+      AND NVL(
+            CAB_CUSTO.CODTIPVENDA,
+            0
+          ) <> 323
+
+      AND CAB_CUSTO.CODPROJ = {{CODPROJ}}
+
+    GROUP BY
+        ITE.NUNOTA
+) CST
        ON CST.NUNOTA = CAB.NUNOTA
 
 WHERE CAB.CODTIPOPER = 1301
 
   /* Exclui tipo de negociação Interno Obras */
-  AND NVL(CAB.CODTIPVENDA, 0) <> 323
+  AND NVL(
+        CAB.CODTIPVENDA,
+        0
+      ) <> 323
 
   AND CAB.CODPROJ = {{CODPROJ}}
+
+/*FILTRO_DTNEG_INICIAL*/
+/*FILTRO_DTNEG_FINAL*/
 
 ORDER BY
     CAB.DTNEG,
